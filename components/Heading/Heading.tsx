7721207282c64a7aaa88a1ad87d27b4d240d1736
Artem Glazyrin <royalbeggars@yandex.ru>
2022-05-17 02:09:08 +0500

    refactor Heading component

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,17 +1,8 @@
-import * as React from 'react';
-import { FunctionComponent } from 'react';
+import React, { FC } from 'react';
 import { HeadingProps } from './Heading.props';
 import style from './Heading.module.css';
- 
-export const Heading: FunctionComponent<HeadingProps> = ({tag, children}): JSX.Element => {
-    switch (tag) {
-        case 'h1':
-            return <h1 className={style.h1}>{children}</h1>;
-        case 'h2':
-            return <h2 className={style.h2}>{children}</h2>;
-        case 'h3':
-            return <h3 className={style.h3}>{children}</h3>;
-        default:
-            return <></>;
-    }
+
+export const Heading: FC<HeadingProps> = ({ tag, children }): JSX.Element => {
+	const Tag = tag || 'h1';
+	return <Tag style={style}>{children}</Tag>;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { Button, Heading, Chip, Typography } from "../components";
 
-
 export default function Home() {
 	return (
 		<>
-			<Heading tag="h1">Наш UI самый лучший</Heading>
+			<Heading>Наш UI самый лучший</Heading>
+			<Heading tag="h2">Наш UI самый лучший дважды</Heading>
+			<Heading tag="h3">Наш UI самый лучший трижды</Heading>
 			<Button>Кнопа</Button>
 			<Button apperance="ghost" arrow='right'>Кнопа 2</Button>
-			<Chip color='primary'>Photoshop</Chip>
-			<Chip>Figma</Chip>
+			<Chip color='primary' size='m'>Photoshop</Chip>
 			<Chip color="ghost" size='m'>Ghost</Chip>
+			<Chip>Figma</Chip>
 			<Chip color="error">Error</Chip>
-			<Chip color="success" size='m'>Success</Chip>
+			<Chip color="success">Success</Chip>
 			<Typography size='m'>Студенты освоят не только hard skills,
 				необходимые для работы веб-дизайнером, но и soft skills — навыки,
 				которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами.
